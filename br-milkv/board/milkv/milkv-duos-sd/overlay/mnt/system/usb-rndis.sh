233278fcd6c4e998369bde6a804ec0abb615819a
Aaron Haun <aaron@haun.guru>
2024-04-22 21:07:41 -0400

    shift ip to 43 to keep away from my home network

--- br-milkv/board/milkv/milkv-duos-sd/overlay/mnt/system/usb-rndis.sh
+++ br-milkv/board/milkv/milkv-duos-sd/overlay/mnt/system/usb-rndis.sh
@@ -30,7 +30,7 @@ sleep 0.5
 /etc/run_usb.sh start rndis >> /tmp/rndis.log 2>&1
 
 sleep 0.5
-ifconfig usb0 192.168.42.1
+ifconfig usb0 192.168.43.1
 
 count=`ps | grep dnsmasq | grep -v grep | wc -l`
 if [ ${count} -lt 1 ] ;then
