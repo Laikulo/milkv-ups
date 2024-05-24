1f81b69b326c766be42a4555b002b76da583e6dc
Aaron Haun <aaron@haun.guru>
2024-05-23 21:09:20 -0400

    More work on extracting patches

--- comparespace/kernel/make-test.sh
+++ comparespace/kernel/make-test.sh
@@ -2,7 +2,15 @@
 
 set -x
 
-if [ -d test-tree ]; then
+if [[ ! -d stock ]]; then
+	echo >&2 "FATAL: Stock tree missing"
+fi
+
+if [[ ! -d milky ]]; then
+	echo >&2 "FATAL: Vendor tree missing"
+fi
+
+if [[ -d test-tree ]]; then
 	rm -rf test-tree
 fi
 
@@ -12,3 +20,10 @@ cp -r adds/* test-tree
 for patchfile in patches/*.diff; do
 	patch -p1 -d test-tree <"$patchfile"	
 done
+
+# Remove the following form the tree, because they polute the diffs
+# probably should exclude instead
+
+diff -qr --no-dereference -X diffgen.ignore test-tree milky > summary.txt
+diff -urpN --no-dereference -X diffgen.ignore test-tree milky > remaining.diff
+
