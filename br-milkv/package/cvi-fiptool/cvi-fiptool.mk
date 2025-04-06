383753ea962f6bb19984c9b3cdd75515296bc1bd
Aaron Haun <aaron@haun.guru>
2025-04-06 01:28:44 -0400

    feat: install for cvi-fiptool

--- br-milkv/package/cvi-fiptool/cvi-fiptool.mk
+++ br-milkv/package/cvi-fiptool/cvi-fiptool.mk
@@ -2,16 +2,10 @@ CVI_FIPTOOL_VERSION = 7f59889c91f7d5d440d6a09aad0209f0aca3d09d
 CVI_FIPTOOL_SITE = $(call github,sophgo,fiptool,$(CVI_FIPTOOL_VERSION))
 HOST_CVI_FIPTOOL_DEPENDENCIES = host-python3
 
-define CVI_FIPTOOL_BUILD_CMDS
-        echo "NOOP"
-endef
-
-define CVI_FIPTOOL_INSTALL_STAGING_CMDS
-        echo "NOOP"
-endef
-
-define CVI_FIPTOOL_INSTALL_TARGET_CMDS
-        echo "NOOP"
+define HOST_CVI_FIPTOOL_INSTALL_CMDS
+	$(INSTALL) -m 755 $(@D)/fiptool $(HOST_DIR)/bin/cvi-fiptool
+	$(INSTALL) -d $(HOST_DIR)/share/firmware/cvi/
+	cp -a $(@D)/data/* $(HOST_DIR)/share/firmware/cvi/
 endef
 
 $(eval $(host-generic-package))
